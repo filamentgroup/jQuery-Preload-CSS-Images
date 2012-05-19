5d99f402614b5d191ac45af665045dd96b0bef6a
trombadan <trombadan@gmail.com>
2012-05-19 12:47:05 -0600

    Added domainCheck (line 89) variable and an if statement (line 90) to prevent parsing of any cross-domain stylesheets.

--- preloadCssImages.jQuery_v5.js
+++ preloadCssImages.jQuery_v5.js
@@ -85,58 +85,62 @@
 					baseURL += '/'; //tack on a / if needed
 				}
 			}
-			if(sheets[sheetIndex].cssRules || sheets[sheetIndex].rules){
-				thisSheetRules = (sheets[sheetIndex].cssRules) ? //->>> http://www.quirksmode.org/dom/w3c_css.html
-					sheets[sheetIndex].cssRules : //w3
-					sheets[sheetIndex].rules; //ie 
-				var ruleIndex = thisSheetRules.length;
-				while(ruleIndex--){
-					if(thisSheetRules[ruleIndex].style && thisSheetRules[ruleIndex].style.cssText){
-						var text = thisSheetRules[ruleIndex].style.cssText;
-						if(text.toLowerCase().indexOf('url') != -1){ // only add rules to the string if you can assume, to find an image, speed improvement
-							cssPile += text; // thisSheetRules[ruleIndex].style.cssText instead of thisSheetRules[ruleIndex].cssText is a huge speed improvement
-						}
-					} else if(thisSheetRules[ruleIndex].styleSheet) {
-						imported.push(thisSheetRules[ruleIndex].styleSheet);
-						w3cImport = true;
-					}
-					
-				}
-			}
-			//parse cssPile for image urls
-			var tmpImage = cssPile.match(/[^\("]+\.(gif|jpg|jpeg|png)/g);//reg ex to get a string of between a "(" and a ".filename" / '"' for opera-bugfix
-			if(tmpImage){
-				var i = tmpImage.length;
-				while(i--){ // handle baseUrl here for multiple stylesheets in different folders bug
-					var imgSrc = (tmpImage[i].charAt(0) == '/' || tmpImage[i].match('://')) ? // protocol-bug fixed
-						tmpImage[i] : 
-						baseURL + tmpImage[i];
-					
-					if(jQuery.inArray(imgSrc, imgUrls) == -1){
-						imgUrls.push(imgSrc);
-					}
-				}
-			}
 			
-			if(!w3cImport && sheets[sheetIndex].imports && sheets[sheetIndex].imports.length) {
-				for(var iImport = 0, importLen = sheets[sheetIndex].imports.length; iImport < importLen; iImport++){
-					var iHref = sheets[sheetIndex].imports[iImport].href;
-					iHref = iHref.split('/');
-					iHref.pop();
-					iHref = iHref.join('/');
-					if (iHref) {
-						iHref += '/'; //tack on a / if needed
+			var domainCheck = baseURL.indexOf(document.domain); // check if stylesheet is cross-domain
+			if (domainCheck>0){
+				if(sheets[sheetIndex].cssRules || sheets[sheetIndex].rules){
+					thisSheetRules = (sheets[sheetIndex].cssRules) ? //->>> http://www.quirksmode.org/dom/w3c_css.html
+						sheets[sheetIndex].cssRules : //w3
+						sheets[sheetIndex].rules; //ie 
+					var ruleIndex = thisSheetRules.length;
+					while(ruleIndex--){
+						if(thisSheetRules[ruleIndex].style && thisSheetRules[ruleIndex].style.cssText){
+							var text = thisSheetRules[ruleIndex].style.cssText;
+							if(text.toLowerCase().indexOf('url') != -1){ // only add rules to the string if you can assume, to find an image, speed improvement
+								cssPile += text; // thisSheetRules[ruleIndex].style.cssText instead of thisSheetRules[ruleIndex].cssText is a huge speed improvement
+							}
+						} else if(thisSheetRules[ruleIndex].styleSheet) {
+							imported.push(thisSheetRules[ruleIndex].styleSheet);
+							w3cImport = true;
+						}
+						
 					}
-					var iSrc = (iHref.charAt(0) == '/' || iHref.match('://')) ? // protocol-bug fixed
-						iHref : 
-						baseURL + iHref;
-					
-					importedSrc.push(iSrc);
-					imported.push(sheets[sheetIndex].imports[iImport]);
+				}
+				//parse cssPile for image urls
+				var tmpImage = cssPile.match(/[^\("]+\.(gif|jpg|jpeg|png)/g);//reg ex to get a string of between a "(" and a ".filename" / '"' for opera-bugfix
+				if(tmpImage){
+					var i = tmpImage.length;
+					while(i--){ // handle baseUrl here for multiple stylesheets in different folders bug
+						var imgSrc = (tmpImage[i].charAt(0) == '/' || tmpImage[i].match('://')) ? // protocol-bug fixed
+							tmpImage[i] : 
+							baseURL + tmpImage[i];
+						
+						if(jQuery.inArray(imgSrc, imgUrls) == -1){
+							imgUrls.push(imgSrc);
+						}
+					}
 				}
 				
-				
-			}
+				if(!w3cImport && sheets[sheetIndex].imports && sheets[sheetIndex].imports.length) {
+					for(var iImport = 0, importLen = sheets[sheetIndex].imports.length; iImport < importLen; iImport++){
+						var iHref = sheets[sheetIndex].imports[iImport].href;
+						iHref = iHref.split('/');
+						iHref.pop();
+						iHref = iHref.join('/');
+						if (iHref) {
+							iHref += '/'; //tack on a / if needed
+						}
+						var iSrc = (iHref.charAt(0) == '/' || iHref.match('://')) ? // protocol-bug fixed
+							iHref : 
+							baseURL + iHref;
+						
+						importedSrc.push(iSrc);
+						imported.push(sheets[sheetIndex].imports[iImport]);
+					}
+					
+					
+				}
+			} // end cross-domain check
 		}//loop
 		if(imported.length){
 			parseCSS(imported, importedSrc);
